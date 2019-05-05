276fefad5f7febc02129a7e205749b07b6d8103c
hpz <1090377416@qq.com>
2019-05-05 16:41:28 +0800

    实现了二维码的识别和教务系统的登入，还差二维码图像的处理和网页的重定向

--- jw_hzau.py
+++ jw_hzau.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 base_url = "http://jw.hzau.edu.cn/"
 aspx_url = "http://jw.hzau.edu.cn/CheckCode.aspx"
+aspx_default2 = "http://jw.hzau.edu.cn/default2.aspx"
 ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
 
 header = {
@@ -18,34 +19,45 @@ header = {
 r = requests.session()
 
 
-def get_post_data(url_base, url_aspx):
+def get_post_data(url_base, url_aspx, aspx_2):
     html = r.get(url_base, headers=header).text
     soup = BeautifulSoup(html, 'html.parser')
     viewState = soup.select('body.login_bg input')[0]['value']
     print(viewState)
     img = r.get(url_aspx, stream=True, headers=header)
-    with open("checkcode.gif",'wb') as f:
+    with open("checkcode.jpg", 'wb') as f:
         f.write(img.content)
 
-    image = Image.open("checkcode.gif")
+    image = Image.open("checkcode.jpg")
+    image = image.convert("L")
     image.show()
+  
+    print(tesserocr.image_to_text(image))
 
-    # user = input("学号")
-    # pwd = input("密码")
-    # checkCode = input("验证码")
-    # post_info = {
+    checkCode = input("验证码")
+    user = input("学号")
+    pwd = input("密码")
+    # user = "2017307211017"
+    # pwd = "2017kshzkjdx"
+    post_info = {
 
-    #     "__VIEWSTATE": viewState,
-    #     "txtUserName": user,
-    #     "Textbox1": "",
-    #     "TextBox2": pwd,
-    #     "txtSecretCode": checkCode,
-    #     "RadioButtonList1": "(unable to decode value)",
-    #     "Button1": "",
-    #     "lbLanguage": "",
-    #     "hidPdrs": "",
-    #     "hidsc": "",
-    # }
+        "__VIEWSTATE": viewState,
+        "txtUserName": user,
+        "Textbox1": "",
+        "TextBox2": pwd,
+        "txtSecretCode": checkCode,
+        "RadioButtonList1": "(unable to decode value)",
+        "Button1": "",
+        "lbLanguage": "",
+        "hidPdrs": "",
+        "hidsc": "",
+    }
+    html_main = r.post(aspx_2, data=post_info, headers=header).text
+    soup_main = BeautifulSoup(html_main, 'html.parser')
+    url_kb = "http://jw.hzau.edu.cn/xskbcx.aspx?xh=2017307211017&xm=%BB%C6%C5%ED%D6%BE&gnmkdm=N121603"
+    html_kb = r.get(url_kb,headers= header)
+    soup_kb = BeautifulSoup(html_kb.content,'html.parser')
+    print(soup_kb)
 
 
-get_post_data(base_url, aspx_url)
+get_post_data(base_url, aspx_url, aspx_default2)
